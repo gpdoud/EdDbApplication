ee56a23929045d26149b0a00b6744f2a4853574d
Greg Doud <gpdoud@gmail.com>
2020-06-26 16:06:44 -0400

    Add project files.

---
+++ EdDbApplication/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+using EdDbLib;
+
+namespace EdDbApplication {
+    class Program {
+        static void Main() {
+            TestConnection();
+        } 
+        static void TestConnection() {
+            var connection = new Connection("localhost", "sqlexpress", "EdDb");
+            var studentsCtrl = new StudentsController(connection);
+
+            var newStudent = new Student() {
+                Firstname = "Fred",
+                Lastname = "Flintstone",
+                StateCode = "SA",
+                SAT = 1000,
+                GPA = 2.5m,
+                MajorId = null
+            };
+            //var itWorked = studentsCtrl.Insert(newStudent);
+
+            var fred = new Student(61) {
+                Firstname = "Fredrick",
+                Lastname = "Flintstone",
+                StateCode = "SA",
+                SAT = 1000,
+                GPA = 2.5m,
+                MajorId = null
+            };
+            //var itWorked = studentsCtrl.Update(fred);
+
+            var itWorked = studentsCtrl.Delete(100);
+            
+            var student = studentsCtrl.GetByPK(10);
+            var noStudent = studentsCtrl.GetByPK(-1);
+            var students = studentsCtrl.GetAll();
+            
+            connection.Close();
+        }
+        static void Test1() {
+
+            var connStr = @"server=localhost\sqlexpress;database=EdDb;trusted_connection=true;";
+            var sqlConn = new SqlConnection(connStr);
+            sqlConn.Open();
+            if(sqlConn.State != ConnectionState.Open) {
+                throw new Exception("Connection did not open!");
+            }
+
+            var sql = "SELECT * From Student;";
+            var sqlCmd = new SqlCommand(sql, sqlConn);
+            var reader = sqlCmd.ExecuteReader();
+            var students = new List<Student>(60);
+            while(reader.Read()) {
+                var id = Convert.ToInt32(reader["Id"]);
+                var student = new Student(id);
+                student.Firstname = reader["Firstname"].ToString();
+                student.Lastname = reader["Lastname"].ToString();
+                student.StateCode = reader["StateCode"].ToString();
+                student.SAT = Convert.ToInt32(reader["SAT"]);
+                student.GPA = Convert.ToDecimal(reader["GPA"]);
+                student.MajorId = null;
+                if(!reader["MajorId"].Equals(DBNull.Value)) {
+                    student.MajorId = Convert.ToInt32(reader["MajorId"]);
+                }
+                students.Add(student);
+            }
+            reader.Close();
+
+            sqlConn.Close();
+        }
+    }
+}
---
+++ EdDbLib/StudentsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EdDbLib {
+    
+    public class StudentsController {
+
+        public Connection Connection { get; private set; } = null;
+
+        public bool Delete(int Id) {
+            var sql = $"DELETE From Student Where Id = {Id}";
+            var sqlCmd = new SqlCommand(sql, Connection.sqlConnection);
+            var rowsAffected = sqlCmd.ExecuteNonQuery();
+            if(rowsAffected != 1) {
+                throw new Exception("Delete return result not 1");
+            }
+            // if we get here, it worked!
+            return true;
+        }
+
+        public bool Update(Student student) {
+            var majorid = (student.MajorId == null) 
+                ? " NULL " 
+                : $" {student.MajorId} ";
+
+            var sql = $"UPDATE Student Set " +
+                        $" Firstname = '{student.Firstname}', " +
+                        $" Lastname = '{student.Lastname}', " +
+                        $" StateCode = '{student.StateCode}', " +
+                        $" SAT = {student.SAT}, " +
+                        $" GPA = {student.GPA}, " +
+                        $" MajorId = {majorid} " +
+                        $" Where Id = {student.Id}; ";
+
+            var sqlCmd = new SqlCommand(sql, Connection.sqlConnection);
+            var rowsAffected = sqlCmd.ExecuteNonQuery();
+            if(rowsAffected != 1) {
+                throw new Exception("Update return result not 1");
+            }
+            // if we get here, it worked!
+            return true;
+        }
+
+        public bool Insert(Student student) {
+            var majorid = (student.MajorId == null)
+                ? " NULL "
+                : $" {student.MajorId} ";
+
+            var sql = $"INSERT Student " +
+                        " (Firstname, Lastname, StateCode, SAT, GPA, MajorId) " +
+                        " VALUES " +
+                        $" ('{student.Firstname}', '{student.Lastname}', " +
+                        $" '{student.StateCode}', {student.SAT}, {student.GPA}, " +
+                        $" {majorid});";
+            var sqlCmd = new SqlCommand(sql, Connection.sqlConnection);
+            var rowsAffected = sqlCmd.ExecuteNonQuery();
+            if(rowsAffected != 1) {
+                throw new Exception("Insert return result not 1");
+            }
+            // if we get here, it worked!
+            return true;
+        }
+
+        public Student GetByPK(int Id) {
+            var sql = $"SELECT * From Student Where Id = {Id}";
+            var sqlCmd = new SqlCommand(sql, Connection.sqlConnection);
+            var reader = sqlCmd.ExecuteReader();
+            if(!reader.HasRows) {
+                reader.Close();
+                return null;
+            }
+            // if we get to here, we found a student
+            reader.Read();
+            var id = Convert.ToInt32(reader["Id"]);
+            var student = new Student(id);
+            student.Firstname = reader["Firstname"].ToString();
+            student.Lastname = reader["Lastname"].ToString();
+            student.StateCode = reader["StateCode"].ToString();
+            student.SAT = Convert.ToInt32(reader["SAT"]);
+            student.GPA = Convert.ToDecimal(reader["GPA"]);
+            student.MajorId = null;
+            if(!reader["MajorId"].Equals(DBNull.Value)) {
+                student.MajorId = Convert.ToInt32(reader["MajorId"]);
+            }
+            reader.Close();
+            return student;
+        }
+
+        public List<Student> GetAll() {
+            var sql = "SELECT * From Student;";
+            var sqlCmd = new SqlCommand(sql, Connection.sqlConnection);
+            var reader = sqlCmd.ExecuteReader();
+            var students = new List<Student>(60);
+            while(reader.Read()) {
+                var id = Convert.ToInt32(reader["Id"]);
+                var student = new Student(id);
+                student.Firstname = reader["Firstname"].ToString();
+                student.Lastname = reader["Lastname"].ToString();
+                student.StateCode = reader["StateCode"].ToString();
+                student.SAT = Convert.ToInt32(reader["SAT"]);
+                student.GPA = Convert.ToDecimal(reader["GPA"]);
+                student.MajorId = null;
+                if(!reader["MajorId"].Equals(DBNull.Value)) {
+                    student.MajorId = Convert.ToInt32(reader["MajorId"]);
+                }
+                students.Add(student);
+            }
+            reader.Close();
+            return students;
+        }
+
+        public StudentsController(Connection connection) {
+            this.Connection = connection;
+        }
+    }
+}
